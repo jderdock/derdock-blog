efbe8ff9a15c97f31026ec855ffe39495e343931
jderdock <jderdock@gmail.com>
2021-03-16 12:37:06 -0400

    minor error fix

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -3,9 +3,9 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
+const ThankYou = () => (
  <Layout>
-  <SEO title="404: Not found" />
+  <SEO title="Contact Success" />
   <section class="px-2 lg:px-0 max-w-3xl mx-auto">
    <h1>Contact</h1>
    <p>Thanks for contacting me!</p>
@@ -13,4 +13,4 @@ const NotFoundPage = () => (
  </Layout>
 )
 
-export default NotFoundPage
+export default ThankYou
