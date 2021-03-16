468715759d398e336c41b7a6260bb3a8f92ec16a
James Derdock <jderdock@prsguitars.com>
2021-03-16 11:46:14 -0400

    move email link on contact to netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,14 +7,41 @@ const ContactPage = () => (
   <SEO title="Contact" description="How to get in touch with James Derdock" />
   <section class="px-2 lg:px-0 max-w-3xl mx-auto">
    <h1>Contact</h1>
-   <p>
-    You can <a href="mailto:james@derdock.com">email me</a> at
-    james@derdock.com.
-   </p>
-   <p>
-    I'm also on <a href="https://twitter.com/derdock">Twitter</a> and{' '}
-    <a href="https://github.com/jderdock">GitHub</a>.
-   </p>
+   <form name="contact" method="POST" data-netlify="true">
+    <p>
+     <label class="text-base m-0 font-semibold font-sans">
+      Your Name:{' '}
+      <input type="text" name="name" class="border border-solid border-black" />
+     </label>
+    </p>
+    <p>
+     <label class="text-base m-0 font-semibold font-sans">
+      Your Email:{' '}
+      <input
+       type="email"
+       name="email"
+       class="border border-solid border-black"
+      />
+     </label>
+    </p>
+    <p>
+     <label class="text-base m-0 font-semibold font-sans">
+      Message:{' '}
+      <textarea
+       name="message"
+       class="border border-solid border-black w-1/2"
+      ></textarea>
+     </label>
+    </p>
+    <p>
+     <button
+      type="submit"
+      class="mt-6 block font-bold font-sans bg-black px-6 py-3 uppercase text-white hover:bg-red-600"
+     >
+      Send
+     </button>
+    </p>
+   </form>
   </section>
  </Layout>
 )
